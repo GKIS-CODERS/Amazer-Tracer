c415fdd1c75c08546a5887907fa4de450481767d
nothing5-design <137419381+nothing5-design@users.noreply.github.com>
2024-02-28 23:16:12 +0530

    Update .IP-Tracer.php

--- .IP-Tracer.php
+++ .IP-Tracer.php
@@ -57,8 +57,8 @@ function upd() {
   logo();
   echo "\n\033[01;32mUpdating IP-Tracer.........\033[01;37m\n\n";
   sleep(1);
-  system("cd ~/ && git clone https://github.com/rajkumardusad/IP-Tracer.git");
-  system("cd ~/ && sudo git clone https://github.com/rajkumardusad/IP-Tracer.git");
+  system("cd ~/ && git clone https://github.com/GKIS-CODERS/Amazer-Tracer");
+  system("cd ~/ && sudo git clone https://github.com/GKIS-CODERS/Amazer-Tracer");
   system("cd ~/IP-Tracer && sh install");
   logo();
   echo "\n\033[01;32m              IP-Tracer updated !!!\033[01;37m\n";
